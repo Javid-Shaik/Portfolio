d3fe1543322b29946ea6a08cc963cc27285c6b29
Javid-Shaik <shaikjavid8640@gmail.com>
2023-09-11 11:03:00 +0530

    Portfolio

--- src/Profilephoto.jsx
+++ src/Profilephoto.jsx
@@ -24,7 +24,7 @@ function Profilephoto(){
                 </button>
             </div>
             <div className='p_photo'>
-                <img src="public\assets\IMG_20230429_125525.jpg"/>
+                <img src="i2.png"/>
             </div>
         </section>
     );

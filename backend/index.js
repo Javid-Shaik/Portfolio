efc181ca98307316060ef9b77e3341100eca167a
Javid-Shaik <shaikjavid8640@gmail.com>
2023-11-09 13:35:44 +0530

    Ready for deployment

--- backend/index.js
+++ backend/index.js
@@ -19,19 +19,19 @@ app.post('/api/send-email', async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: 'Gmail', // e.g., Gmail, Outlook, etc.
     auth: {
-      user: 'shaikjavidbasha528@gmail.com',
+      user: process.env.EMAIL,
       pass: process.env.PASSWORD
     }
   });
 
   const mailOptions = {
-    from: 'shaikjavidbasha528@gmail.com', // Sender's email
-    to: 'shaikjavidbasha528@gmail.com', // Recipient's email
+    from: process.env.EMAIL, // Sender's email
+    to: process.env.EMAIL, // Recipient's email
     subject: subject,
     text: `From: ${fname} ${lname}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`
   };
   const confirmationMailOptions = {
-    from: 'shaikjavidbasha528@gmail.com',
+    from: process.env.EMAIL,
     to: email, // Viewer's email address
     subject: 'Message Received Confirmation',
     text: `Hello ${fname},\n\nThank you for your message. We have received it and will get back to you soon.\n\nBest regards,\nShaik Javid Basha`
@@ -64,6 +64,7 @@ app.post('/api/send-email', async (req, res) => {
 const __dirname1 = path.resolve()
 if(process.env.NODE_ENV=='production'){
   app.use(express.static(path.join(__dirname1 , "/dist")));
+
   app.get("*", (req, res)=>{
     res.sendFile(path.resolve(__dirname1, "dist", "index.html"))
   })
